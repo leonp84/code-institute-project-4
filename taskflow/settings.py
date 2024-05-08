f68597746626b5dd23f8f83a1bc40f3870789cf3
Leon Potgieter <hexvallei@gmail.com>
2024-05-08 20:04:55 +0000

    feat: add user accounts forms for login and logout to resolve #5

--- taskflow/settings.py
+++ taskflow/settings.py
@@ -30,7 +30,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Django Allauth Settings
 
 SITE_ID = 1
-LOGIN_REDIRECT_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
